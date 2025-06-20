2089a09cd86771dd2a7eb8a7805e13d96e44a066
Fabiana Monteiro Menocci da Silva <fabi_mms_05@hotmail.com>
2025-06-20 11:43:02 -0300

    Iniciando o projeto do meu portfolio em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderSectionComponent } from "./components/header-section/header-section.component";
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { ServicesSectionComponent } from "./components/services-section/services-section.component";
+import { SkillsSectionComponent } from "./components/skills-section/skills-section.component";
+import { PortfolioSectionComponent } from "./components/portfolio-section/portfolio-section.component";
+import { ContactSectionComponent } from "./components/contact-section/contact-section.component";
+import { FooterSectionComponent } from "./components/footer-section/footer-section.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderSectionComponent, HeroSectionComponent, ServicesSectionComponent, SkillsSectionComponent, PortfolioSectionComponent, ContactSectionComponent, FooterSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/header-section/header-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-section',
+  imports: [],
+  templateUrl: './header-section.component.html',
+  styleUrl: './header-section.component.scss'
+})
+export class HeaderSectionComponent {
+
+}
